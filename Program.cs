3d4f17c8790b4f9b0eeae0c4481e530bc44e2b0a
erce04 <112382868+erce04@users.noreply.github.com>
2022-10-17 08:30:41 +0200

    Gjort klart spelet

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Random otal = new Random();
+int y = otal.Next(1,100);
+int gissning = 0;
+Console.WriteLine("Gissa det okända talet mellan 1-100");
+int summa = 1;
+while (gissning != y)
+{
+    gissning = Int32.Parse(Console.ReadLine());
+    if (gissning < y)
+    {
+        Console.WriteLine("Din gissning var för låg");
+        summa++;
+    }
+    else if (gissning > y)
+    {
+        Console.WriteLine("Din gissning var för hög");
+        summa++;
+    }
+}
+Console.WriteLine("Du gissade rätt tal: " + y + " på " + summa + " försök");
